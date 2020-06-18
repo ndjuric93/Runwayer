67664fd5732c267731900710eb3827ef3304823c
Nemanja Djuric <nemanja.djuric@outlook.com>
2020-06-19 00:24:05 +0200

    Add rotating runway

---
+++ runwayer/src/App.vue
@@ -0,0 +1,128 @@
+<template>
+<div id="container" >
+<div :style="style">
+<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 130.55 1014.41"
+  width="100" height="500"
+>
+    <defs>
+        
+        <clipPath id="clip-path">
+            <rect class="cls-1" width="130.55" height="1014.41" />
+        </clipPath>
+    </defs>
+    <rect class="cls-2" width="130.55" height="1014.41" />
+    <g class="cls-3">
+        <polyline class="cls-4" points="156.1 30.44 64.96 90.91 -23.9 30.44" />
+        <polyline class="cls-4" points="156.1 7.81 64.96 68.28 -23.9 7.81" />
+        <polyline class="cls-4" points="156.1 -14.82 64.96 45.65 -23.9 -14.82" />
+        <polyline class="cls-4" points="156.1 -37.46 64.96 23.02 -23.9 -37.46" />
+        <polyline class="cls-4" points="156.1 53.07 64.96 113.54 -23.9 53.07" />
+        <polyline class="cls-4" points="-24.77 986.18 66.37 925.71 155.23 986.18" />
+        <polyline class="cls-4" points="-24.77 1008.82 66.37 948.34 155.23 1008.82" />
+        <polyline class="cls-4" points="-24.77 1031.45 66.37 970.97 155.23 1031.45" />
+        <polyline class="cls-4" points="-24.77 1054.08 66.37 993.6 155.23 1054.08" />
+        <polyline class="cls-4" points="-24.77 963.55 66.37 903.08 155.23 963.55" />
+    </g>
+    <rect class="cls-5" x="5.99" y="91.11" width="118.57" height="832.57" />
+    <rect class="cls-6" x="5.99" y="803.93" width="118.57" height="74.74" />
+    <rect class="cls-6" x="5.99" y="120.33" width="118.57" height="74.74" />
+    <line class="cls-7" x1="16.06" y1="809.8" x2="16.06" y2="872.8" />
+    <line class="cls-7" x1="28.19" y1="809.8" x2="28.19" y2="872.8" />
+    <line class="cls-7" x1="40.32" y1="809.8" x2="40.32" y2="872.8" />
+    <line class="cls-7" x1="52.45" y1="809.8" x2="52.45" y2="872.8" />
+    <line class="cls-7" x1="78.1" y1="809.8" x2="78.1" y2="872.8" />
+    <line class="cls-7" x1="90.23" y1="809.8" x2="90.23" y2="872.8" />
+    <line class="cls-7" x1="102.36" y1="809.8" x2="102.36" y2="872.8" />
+    <line class="cls-7" x1="114.49" y1="809.8" x2="114.49" y2="872.8" />
+    <polyline class="cls-8" points="52.62 907.22 65.1 894.75 77.93 907.57" />
+    <polyline class="cls-8" points="52.62 897.01 65.1 884.53 77.93 897.36" />
+    <polyline class="cls-8" points="52.62 917.56 65.1 905.08 77.93 917.9" />
+    <text class="cls-9" transform="translate(42.93 798.46)">{{this.runwayDirection.toString().padStart(2, '0')}}</text>
+    <line class="cls-10" x1="65.27" y1="741.46" x2="65.27" y2="691.37" />
+    <line class="cls-10" x1="65.27" y1="652.24" x2="65.27" y2="602.15" />
+    <line class="cls-10" x1="65.27" y1="563.02" x2="65.27" y2="512.93" />
+    <line class="cls-10" x1="65.27" y1="473.8" x2="65.27" y2="423.72" />
+    <line class="cls-10" x1="65.27" y1="384.59" x2="65.27" y2="334.5" />
+    <line class="cls-10" x1="65.27" y1="295.37" x2="65.27" y2="245.28" />
+    <line class="cls-7" x1="28.35" y1="375.46" x2="28.35" y2="336.36" />
+    <line class="cls-7" x1="40.77" y1="375.46" x2="40.77" y2="336.36" />
+    <line class="cls-7" x1="28.35" y1="529.37" x2="28.35" y2="490.27" />
+    <line class="cls-7" x1="40.77" y1="529.37" x2="40.77" y2="490.27" />
+    <line class="cls-7" x1="28.35" y1="683.28" x2="28.35" y2="644.18" />
+    <line class="cls-7" x1="40.77" y1="683.28" x2="40.77" y2="644.18" />
+    <line class="cls-7" x1="89.78" y1="375.46" x2="89.78" y2="336.36" />
+    <line class="cls-7" x1="102.2" y1="375.46" x2="102.2" y2="336.36" />
+    <line class="cls-7" x1="89.78" y1="529.37" x2="89.78" y2="490.27" />
+    <line class="cls-7" x1="102.2" y1="529.37" x2="102.2" y2="490.27" />
+    <line class="cls-7" x1="89.78" y1="683.28" x2="89.78" y2="644.18" />
+    <line class="cls-7" x1="102.2" y1="683.28" x2="102.2" y2="644.18" />
+    <text class="cls-9" transform="translate(87.62 202.69) rotate(180)">{{this.oppositeDirection.toString().padStart(2, '0')}}</text>
+    <line class="cls-7" x1="16.06" y1="126.2" x2="16.06" y2="189.2" />
+    <line class="cls-7" x1="28.19" y1="126.2" x2="28.19" y2="189.2" />
+    <line class="cls-7" x1="40.32" y1="126.2" x2="40.32" y2="189.2" />
+    <line class="cls-7" x1="52.45" y1="126.2" x2="52.45" y2="189.2" />
+    <line class="cls-7" x1="78.1" y1="126.2" x2="78.1" y2="189.2" />
+    <line class="cls-7" x1="90.23" y1="126.2" x2="90.23" y2="189.2" />
+    <line class="cls-7" x1="102.36" y1="126.2" x2="102.36" y2="189.2" />
+    <line class="cls-7" x1="114.49" y1="126.2" x2="114.49" y2="189.2" />
+    <polyline class="cls-8" points="37.64 100.1 25.16 112.57 12.34 99.75" />
+    <polyline class="cls-8" points="77.93 100.1 65.45 112.57 52.62 99.75" />
+    <polyline class="cls-8" points="118.21 100.1 105.74 112.57 92.91 99.75" />
+
+</svg>
+</div>
+<button @click="rotate">
+  Rotate
+</button>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import {InlineSvgPlugin} from 'vue-inline-svg';
+Vue.use(InlineSvgPlugin);
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      runwayDirection: 1,
+      oppositeDirection: 36
+    }
+  },
+  methods: {
+    rotate: function() {
+      this.runwayDirection = this.getRandomInt(36)
+      this.oppositeDirection = (this.runwayDirection + 18) % 36
+    },
+    getRandomInt:function (max) {
+      return  1 + Math.floor(Math.random() * Math.floor(max));
+    }
+  },
+  computed: {
+    style() {
+      return { transform: 'rotate(' + this.runwayDirection + 'deg )'}
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#container {
+  top: 10%;
+  left: 50%;
+  position: absolute;
+}
+
+.cls-1,.cls-10,.cls-4,.cls-6,.cls-7,.cls-8 {fill:none;}.cls-2.cls-5 {fill:#303030;}.cls-3 {clip-path:url(#clip-path);}.cls-4 {stroke:#8c5617;stroke-width:5px;}.cls-10,.cls-5,.cls-6,.cls-7,.cls-8{stroke-miterlimit:10;}.cls-10,.cls-5,.cls-6,.cls-7,.cls-8{stroke:#fcfcfc;}.cls-5,.cls-8{stroke-width:3px;}.cls-6{stroke-width:2px;}.cls-7{stroke-width:6px;}.cls-8{stroke-linecap:round;}.cls-9{font-size:39.8px;fill:#fcfcfc;font-family:Roboto-Regular, Roboto;}.cls-10{stroke-width:4px;}
+
+</style>
